65e32e51b1b7e9b20ed63690e95d2f9b9307f66c
Ali Dikici <arzdkc@outlook.com>
2026-05-09 09:35:16 +0300

    testing types extended

---
+++ testing/types/istanbul-lib-coverage.d.ts
@@ -0,0 +1,22 @@
+declare module 'istanbul-lib-coverage' {
+  type FileCoverage = {
+    path?: string;
+    statementMap?: unknown;
+    [key: string]: unknown;
+  };
+
+  export type CoverageMapData = Record<string, FileCoverage>;
+
+  export interface CoverageMap {
+    merge(other: CoverageMapData | CoverageMap): void;
+    toJSON(): CoverageMapData;
+  }
+
+  export function createCoverageMap(initialCoverage?: CoverageMapData): CoverageMap;
+
+  const defaultExport: {
+    createCoverageMap: typeof createCoverageMap;
+  };
+
+  export default defaultExport;
+}
